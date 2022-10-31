4ba4c23565e2ba4c7f368ad44886bdcb22c97761
Bailym <bm39@hotmail.co.uk>
2022-10-31 21:55:00 +0000

    layout home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,6 @@
 import './Home.css';
 import React, { PureComponent, useState, useEffect } from 'react';
-import { Input, Button } from '@chakra-ui/react'
+import { Input, Button, Flex, Spacer, Box } from '@chakra-ui/react'
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import axios from 'axios';
 
@@ -24,31 +24,21 @@ function Home() {
 
 
   return (
-    <div>
-      <p>Home</p>
-      <Input placeholder="Poll ID" id="poll-id"></Input>
-      <Button onClick={() => getPoll()}>Get Poll</Button>
-      <ResponsiveContainer width="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="Party" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="Points" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+    <Flex>
+      <Box flex="2" height="92vh" margin="2vh" bgColor="#E5F3F4">
+
+      </Box>
+      <Box flex="4" margin="2vh">
+        <Flex height="92vh" flexDir="column" justify="center">
+          <Box flex="1" margin="0 1vw 1vw 1vw" bgColor="#E5F3F4">
+
+          </Box>
+          <Box flex="1" margin="1vw 1vw 0 1vw" bgColor="#E5F3F4">
+
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
   );
 }
 
