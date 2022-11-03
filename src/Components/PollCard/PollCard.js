e110188ac3e512724075189a7a6dd454ce7277ac
Bailym <bm39@hotmail.co.uk>
2022-11-03 21:08:57 +0000

    add source and date from props and improved styling

--- src/Components/PollCard/PollCard.js
+++ src/Components/PollCard/PollCard.js
@@ -1,39 +1,37 @@
-import { Container, Box, Flex, Text } from '@chakra-ui/react'
-import { useEffect, useState, PureComponent } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {Box, Flex, Text } from '@chakra-ui/react'
+import Moment from 'react-moment';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 
 function PollCard(props) {
     return (
-        <div style={{background:"rgba(245, 245, 245, 0.75)", boxShadow:"0px 4px 4px 5px rgba(0, 0, 0, 0.1)", width:"90%", maxHeight:"30%", margin:"1.5rem auto"}}>
-            {<Flex padding="1rem">
-                <Box flexDir="row" flex="1">
-                    <Text>Opinium Research</Text>
+        <div style={{ background: "rgba(245, 245, 245, 0.75)", boxShadow: "0px 4px 4px 5px rgba(0, 0, 0, 0.1)", width: "90%", height: "30%", margin: "1.5rem auto" }}>
+            <Flex>
+                <Box flexDir="row" flex="1" margin="1rem">
+                    <Text>{props.data.Source}</Text>
                 </Box>
-                <Box flex="1">
-                    <Text align="right">29/12/2022</Text>
+                <Box flex="1" margin="1rem">
+                    <Text align="right"><Moment format='DD/MM/YYYY'>{props.data.DatePublished}</Moment></Text>
                 </Box>
-            </Flex>}
+            </Flex>
             <ResponsiveContainer>
                 <BarChart
-                    width="100%"
-                    height="100%"
-                    data={props.data.Data}
                     margin={{
-                        top: 20,
+                        top: 5,
                         right: 30,
-                        left: 20,
-                        bottom: 0,
+                        left: 30,
+                        bottom: 60,
                     }}
-                >
+                    data={props.data.Data}>
                     <XAxis dataKey="Party" />
                     <YAxis />
                     <Tooltip />
                     <Bar dataKey="Points" fill="#82ca9d" />
                 </BarChart>
             </ResponsiveContainer>
-        </div>)
+        </div>
+    )
 }
 
 export default PollCard;
