6a90f5774ab29efe20bb178eea104bada485cc2b
Bailym <bm39@hotmail.co.uk>
2022-11-03 20:32:17 +0000

    style pollcard according to design with placeholders

--- src/Components/PollCard/PollCard.js
+++ src/Components/PollCard/PollCard.js
@@ -1,4 +1,4 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Box, Flex, Text } from '@chakra-ui/react'
 import { useEffect, useState, PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
@@ -6,24 +6,35 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
 
 function PollCard(props) {
     return (
-        <ResponsiveContainer width="100%" height="25%">
-            <BarChart
-                width="100%"
-                height="100%"
-                data={props.data.Data}
-                margin={{
-                    top: 20,
-                    right: 30,
-                    left: 20,
-                    bottom: 0,
-                }}
-            >
-                <XAxis dataKey="Party" />
-                <YAxis />
-                <Tooltip />
-                <Bar dataKey="Points" fill="#82ca9d" />
-            </BarChart>
-        </ResponsiveContainer>)
+        <div style={{background:"rgba(245, 245, 245, 0.75)", boxShadow:"0px 4px 4px 5px rgba(0, 0, 0, 0.1)", width:"90%", maxHeight:"30%", margin:"1.5rem auto"}}>
+            {<Flex padding="1rem">
+                <Box flexDir="row" flex="1">
+                    <Text>Opinium Research</Text>
+                </Box>
+                <Box flex="1">
+                    <Text align="right">29/12/2022</Text>
+                </Box>
+            </Flex>}
+            <ResponsiveContainer>
+                <BarChart
+                    width="100%"
+                    height="100%"
+                    data={props.data.Data}
+                    margin={{
+                        top: 20,
+                        right: 30,
+                        left: 20,
+                        bottom: 0,
+                    }}
+                >
+                    <XAxis dataKey="Party" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="Points" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>)
 }
 
 export default PollCard;
+
