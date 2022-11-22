0cb09311e7ae2142ec9e5b45917e1407beb37fc8
Bailym <bm39@hotmail.co.uk>
2022-11-22 19:51:37 +0000

    format modal dates

--- src/Components/PollCardModal/PollCardModal.js
+++ src/Components/PollCardModal/PollCardModal.js
@@ -1,19 +1,14 @@
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, ListIcon, List, ListItem, Text } from '@chakra-ui/react'
 import { CalendarIcon, Search2Icon, UpDownIcon, DragHandleIcon } from '@chakra-ui/icons'
 import { useEffect, useState } from 'react'
+import Moment from 'react-moment';
 
 function PollCardModal(props) {
 
     const [data, setData] = useState(props.data)
 
-    useEffect(() => {
-
-        console.log(data)
-    })
-
-
     return (<>
-        <Modal isOpen={props.isOpen} onClose={props.onClose}>
+        <Modal isOpen={props.isOpen} onClose={props.onClose} size="xs">
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>Poll Details</ModalHeader>
@@ -26,19 +21,19 @@ function PollCardModal(props) {
                         </ListItem>
                         <ListItem>
                             <ListIcon as={CalendarIcon} color='green.500' />
-                            <Text display="inline" >Date Published: {data.DatePublished} </Text>
+                            <Text display="inline" >Date Published: <Moment format ="DD/MM/YYYY">{data.DatePublished}</Moment></Text>
                         </ListItem>
                         <ListItem>
                             <ListIcon as={CalendarIcon} color='green.500' />
-                            <Text display="inline" >Start Date: {data.SurveyDate.StartDate}</Text>
+                            <Text display="inline" >Start Date: <Moment format ="DD/MM/YYYY">{data.SurveyDate.StartDate}</Moment></Text>
                         </ListItem>
                         <ListItem>
                             <ListIcon as={CalendarIcon} color='green.500' />
-                            <Text display="inline" >End Date: {data.SurveyDate.EndDate} </Text>
+                            <Text display="inline" >End Date: <Moment format ="DD/MM/YYYY">{data.SurveyDate.EndDate}</Moment></Text>
                         </ListItem>
                         <ListItem>
                             <ListIcon as={UpDownIcon} color='green.500' />
-                            <Text display="inline" >Changes With: {data.ChangesWith} </Text>
+                            <Text display="inline" >Changes With: <Moment format ="DD/MM/YYYY">{data.ChangesWith}</Moment> </Text>
                         </ListItem>
                         <ListItem>
                             <ListIcon as={DragHandleIcon} color='green.500' />
