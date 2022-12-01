221e8a195742e48f81948bfdf3ecb92c4a02e327
Bailym <bm39@hotmail.co.uk>
2022-12-01 21:52:18 +0000

    use relative path for incon

--- src/Components/HeaderBar/HeaderBar.js
+++ src/Components/HeaderBar/HeaderBar.js
@@ -4,7 +4,7 @@ function HeaderBar() {
 
     return (
         <Box bgColor="#32D7B9" flexDirection="row" height="5vh" >
-            <Link href="/" display="block" maxWidth="64px"><Image src={process.env.PUBLIC_URL + "icon.png"} alt="logo" maxH="5vh" maxWidth="64px" /></Link>
+            <Link href="/" display="block" maxWidth="64px"><Image src={"icon.png"} alt="logo" maxH="5vh" maxWidth="64px" /></Link>
         </Box>
 
     )
