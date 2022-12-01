2b1f01cd412027e12a607d963e537c67fb9c6c39
Bailym <bm39@hotmail.co.uk>
2022-12-01 21:13:19 +0000

    add run script

---
+++ run.js
@@ -0,0 +1,26 @@
+/**
+ * Frontend react.js server start up script.
+ */
+
+// Load .env file
+
+require('dotenv').config();
+
+let server = require("./backend/server");
+let app = server.app;
+let port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(createProxyMiddleware('/api', { target: "http://localhost:3001"}));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
